e792598bf56c3dc321890764bfdc2eca85f056e1
sebastian <imanchesebastian@gmail.com>
2022-01-20 12:21:04 +0100

    implemented observer pattern with classes

---
+++ observer-pattern.js
@@ -0,0 +1,68 @@
+class Observer {
+  update(event) {
+    if (this.eventIsRelevant(event)) {
+      this.reactToEvent(event);
+    }
+  }
+
+  eventIsRelevant(event) {
+    throw new Error("Must be implemented");
+  }
+
+  reactToEvent(event) {
+    throw new Error("Must be implemented");
+  }
+}
+
+class Subject {
+  constructor() {
+    this.observers = [];
+  }
+
+  addObserver(obj) {
+    this.observers.push(obj);
+  }
+
+  notifyObservers(event) {
+    this.observers.forEach((obs) => obs.update(event));
+  }
+}
+
+class OddModifier extends Observer {
+  constructor() {
+    super();
+  }
+
+  eventIsRelevant(event) {
+    return event.eventName === "change-number" && event.value % 2 != 0;
+  }
+
+  reactToEvent(event) {
+    console.log("----------------------");
+    console.log("Odd number found!");
+    console.log(event.value);
+    console.log("----------------------");
+  }
+}
+
+class Looper extends Subject {
+  constructor(start, end) {
+    super();
+    this.start = start;
+    this.current = start;
+    this.end = end;
+  }
+
+  run() {
+    for (this.current = this.start; this.current < this.end; this.current++) {
+      this.notifyObservers({
+        eventName: "change-number",
+        value: this.current,
+      });
+    }
+  }
+}
+
+const logger = new Looper(1, 1000);
+logger.addObserver(new OddModifier());
+logger.run();
